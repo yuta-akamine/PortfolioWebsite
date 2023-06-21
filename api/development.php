2bfafc91ac3c23c98d7beb1e3b8e60cfacdf1041
yuta-akamine <happykaizokuou@gmail.com>
2023-06-21 15:59:28 +0900

    🎨 トグルリスト、使用言語、アプリの説明変更

--- api/development.php
+++ api/development.php
@@ -16,12 +16,19 @@ include(dirname ( __FILE__ ) . '/header.html'); //ヘッダーを読み込み
 
       </div>  <!-- <div class="row"> -->
 
-      <dt class="developbox">コスパ計算アプリ:</dt>
-      <dd>
-        <a href="https://thawing-ridge-19896.herokuapp.com/" target=_blank>https://thawing-ridge-19896.herokuapp.com/</a><br>
-        <p>保険や車代などの比較的大きな出費から日用品や食品など様々なコスパの計算ができます</p>
+      <details>
+        <summary>
+          <p class="toguru">Webアプリケーション</p>
+          <!-- <h4 class="toguru">Webアプリケーション</h4> -->
+        </summary>
+        <dt class="developbox">コスパ計算アプリ:</dt>
+        <dd>
+          <a href="https://thawing-ridge-19896.herokuapp.com/" target=_blank>https://thawing-ridge-19896.herokuapp.com/</a><br>
+        <dt class="developbox">利用言語:</dt>
+        <dd>HTML、CSS、Bootstrap、JavaScript、PHP</dd>
+        <p>保険や車代など年間あたりの購入単価や、日用品や食品の購入単価などの計算サイトです。</p>
 
-      </dd>
+      </details>
 
     </section>
     
