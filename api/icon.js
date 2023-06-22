fb0fdd1eb1e31bd5fcdd058d0118b49c74c2360f
yuta-akamine <happykaizokuou@gmail.com>
2023-06-22 19:32:24 +0900

    🎨 変数名変更

--- api/icon.js
+++ api/icon.js
@@ -1,68 +1,71 @@
+// Strict Mode
+'use strict';
+
 // html
-const html = document.getElementById("html");
-const htmltooltip = document.getElementById("htmlExplanation");
+const html = document.getElementById("html"); //imgタグ取得
+const htmlTooltip = document.getElementById("htmlExplanation"); //説明用タグ取得
 
 // css
 const css = document.getElementById("css");
-const csstooltip = document.getElementById("cssExplanation");
+const cssTooltip = document.getElementById("cssExplanation");
 
 // JavaScript
 const js = document.getElementById("js");
-const jstooltip = document.getElementById("jsExplanation");
+const jsTooltip = document.getElementById("jsExplanation");
 
 // PHP
 const php = document.getElementById("php");
-const phptooltip = document.getElementById("phpExplanation");
+const phpTooltip = document.getElementById("phpExplanation");
 
 // Java
 const java = document.getElementById("java");
-const javatooltip = document.getElementById("javaExplanation");
+const javaTooltip = document.getElementById("javaExplanation");
 
 // MySQL
 const mysql = document.getElementById("mysql");
-const mysqltooltip = document.getElementById("mysqlExplanation");
+const mysqlTooltip = document.getElementById("mysqlExplanation");
 
 // Heroku
 const heroku = document.getElementById("heroku");
-const herokutooltip = document.getElementById("herokuExplanation");
+const herokuTooltip = document.getElementById("herokuExplanation");
 
 // VSCode
 const vscode = document.getElementById("vscode");
-const vscodetooltip = document.getElementById("vscodeExplanation");
+const vscodeTooltip = document.getElementById("vscodeExplanation");
 
 // Eclipse
 const eclipse = document.getElementById("eclipse");
-const eclipsetooltip = document.getElementById("eclipseExplanation");
+const eclipseTooltip = document.getElementById("eclipseExplanation");
 
 // Android Studio
 const android = document.getElementById("android");
-const androidtooltip = document.getElementById("androidExplanation");
+const androidTooltip = document.getElementById("androidExplanation");
 
 // GitHub
 const github = document.getElementById("github");
-const githubtooltip = document.getElementById("githubExplanation");
+const githubTooltip = document.getElementById("githubExplanation");
 
 // virtualbox
 const virtualbox = document.getElementById("virtualbox");
-const virtualboxtooltip = document.getElementById("virtualboxExplanation");
+const virtualboxTooltip = document.getElementById("virtualboxExplanation");
 
 // VMware
 const vmware = document.getElementById("vmware");
-const vmwaretooltip = document.getElementById("vmwareExplanation");
+const vmwareTooltip = document.getElementById("vmwareExplanation");
 
 // slack
 const slack = document.getElementById("slack");
-const slacktooltip = document.getElementById("slackExplanation");
+const slackTooltip = document.getElementById("slackExplanation");
 
 // notion
 const notion = document.getElementById("notion");
-const notiontooltip = document.getElementById("notionExplanation");
+const notionTooltip = document.getElementById("notionExplanation");
+
 
 // マウスホバー時のイベントリスナーを追加
-
 // html
-html.addEventListener("mouseover", htmlshowTooltip);
-html.addEventListener("mouseout", hideTooltip);
+html.addEventListener("mouseover", htmlshowTooltip); //imgタグにマウスホバー時
+html.addEventListener("mouseout", hideTooltip); //imgタグからマウスホバー解除時
 
 // css
 css.addEventListener("mouseover", cssshowTooltip);
@@ -121,137 +124,136 @@ notion.addEventListener("mouseover", notionshowTooltip);
 notion.addEventListener("mouseout", hideTooltip);
 
 // ツールチップを表示する関数(html)
-function htmlshowTooltip(event) {
+function htmlshowTooltip(event) { //マウスホバー時の処理
   // マウスの位置にツールチップを表示
-  htmltooltip.style.display = "inline";  // html
+  htmlTooltip.style.display = "inline";  // html
   // tooltip.style.top = (event.clientY + 20) + "px";
   // tooltip.style.left = (event.clientX + 20) + "px";
 
   // 説明文をセット
-  htmltooltip.innerText = "HTML5";  // html
+  htmlTooltip.innerText = "HTML5";  //htmlの説明文
 }
 
 // ツールチップを表示する関数(css)
 function cssshowTooltip(event) {
   // マウスの位置にツールチップを表示
-  csstooltip.style.display = "inline";
-  csstooltip.innerText = "CSS3";
+  cssTooltip.style.display = "inline";
+  cssTooltip.innerText = "CSS3";
 }
 
 // ツールチップを表示する関数(JavaScript)
 function jsshowTooltip(event) {
   // マウスの位置にツールチップを表示
-  jstooltip.style.display = "inline";
-  jstooltip.innerText = "JavaScript";
+  jsTooltip.style.display = "inline";
+  jsTooltip.innerText = "JavaScript";
 }
 
 // ツールチップを表示する関数(PHP)
 function phpshowTooltip(event) {
   // マウスの位置にツールチップを表示
-  phptooltip.style.display = "inline";
-  phptooltip.innerText = "PHP";
+  phpTooltip.style.display = "inline";
+  phpTooltip.innerText = "PHP";
 }
 
 // ツールチップを表示する関数(Java)
 function javashowTooltip(event) {
   // マウスの位置にツールチップを表示
-  javatooltip.style.display = "inline";
-  javatooltip.innerText = "Java";
+  javaTooltip.style.display = "inline";
+  javaTooltip.innerText = "Java";
 }
 
 // ツールチップを表示する関数(MySQL)
 function mysqlshowTooltip(event) {
   // マウスの位置にツールチップを表示
-  mysqltooltip.style.display = "inline";
-  mysqltooltip.innerText = "MySQL";
+  mysqlTooltip.style.display = "inline";
+  mysqlTooltip.innerText = "MySQL";
 }
 
 // ツールチップを表示する関数(Heroku)
 function herokushowTooltip(event) {
   // マウスの位置にツールチップを表示
-  herokutooltip.style.display = "inline";
-  herokutooltip.innerText = "Heroku";
+  herokuTooltip.style.display = "inline";
+  herokuTooltip.innerText = "Heroku";
 }
 
 // ツールチップを表示する関数(Visual Studio Code)
 function vscodeshowTooltip(event) {
   // マウスの位置にツールチップを表示
-  vscodetooltip.style.display = "inline";
-  vscodetooltip.innerText = "Visual Studio Code";
+  vscodeTooltip.style.display = "inline";
+  vscodeTooltip.innerText = "Visual Studio Code";
 }
 
 // ツールチップを表示する関数(Heroku)
 function herokushowTooltip(event) {
   // マウスの位置にツールチップを表示
-  herokutooltip.style.display = "inline";
-  herokutooltip.innerText = "Heroku";
+  herokuTooltip.style.display = "inline";
+  herokuTooltip.innerText = "Heroku";
 }
 
 // ツールチップを表示する関数(Eclipse)
 function eclipseshowTooltip(event) {
   // マウスの位置にツールチップを表示
-  eclipsetooltip.style.display = "inline";
-  eclipsetooltip.innerText = "Eclipse";
+  eclipseTooltip.style.display = "inline";
+  eclipseTooltip.innerText = "Eclipse";
 }
 
 // ツールチップを表示する関数(Android)
 function androidshowTooltip(event) {
   // マウスの位置にツールチップを表示
-  androidtooltip.style.display = "inline";
-  androidtooltip.innerText = "Android";
+  androidTooltip.style.display = "inline";
+  androidTooltip.innerText = "Android Studio";
 }
 
 // ツールチップを表示する関数(GitHub)
 function githubshowTooltip(event) {
   // マウスの位置にツールチップを表示
-  githubtooltip.style.display = "inline";
-  githubtooltip.innerText = "GitHub";
+  githubTooltip.style.display = "inline";
+  githubTooltip.innerText = "GitHub";
 }
 
 // ツールチップを表示する関数(virtualbox)
 function virtualboxshowTooltip(event) {
   // マウスの位置にツールチップを表示
-  virtualboxtooltip.style.display = "inline";
-  virtualboxtooltip.innerText = "Virtualbox";
+  virtualboxTooltip.style.display = "inline";
+  virtualboxTooltip.innerText = "Virtualbox";
 }
 
 // ツールチップを表示する関数(vmware)
 function vmwareshowTooltip(event) {
   // マウスの位置にツールチップを表示
-  vmwaretooltip.style.display = "inline";
-  vmwaretooltip.innerText = "VMware";
+  vmwareTooltip.style.display = "inline";
+  vmwareTooltip.innerText = "VMware";
 }
 
 // ツールチップを表示する関数(Slack)
 function slackshowTooltip(event) {
   // マウスの位置にツールチップを表示
-  slacktooltip.style.display = "inline";
-  slacktooltip.innerText = "Slack";
+  slackTooltip.style.display = "inline";
+  slackTooltip.innerText = "Slack";
 }
 
 // ツールチップを表示する関数(Notion)
 function notionshowTooltip(event) {
   // マウスの位置にツールチップを表示
-  notiontooltip.style.display = "inline";
-  notiontooltip.innerText = "Notion";
+  notionTooltip.style.display = "inline";
+  notionTooltip.innerText = "Notion";
 }
 
 // ツールチップを非表示にする関数
-function hideTooltip() {
-  // html
-  htmltooltip.style.display = "none";  // html
-  csstooltip.style.display = "none";  // CSS
-  jstooltip.style.display = "none";  // JavaScript
-  phptooltip.style.display = "none";  // php
-  javatooltip.style.display = "none";  // Java
-  mysqltooltip.style.display = "none";  // MySQL
-  herokutooltip.style.display = "none";  // Heroku
-  vscodetooltip.style.display = "none";  // VSCode
-  eclipsetooltip.style.display = "none";  // Eclipse
-  androidtooltip.style.display = "none";  // Android
-  githubtooltip.style.display = "none";  // github
-  virtualboxtooltip.style.display = "none";  // virtualbox
-  vmwaretooltip.style.display = "none";  // vmware
-  slacktooltip.style.display = "none";  // Slack
-  notiontooltip.style.display = "none";  // Notion
+function hideTooltip() { //ホバー解除時
+  htmlTooltip.style.display = "none";  // html
+  cssTooltip.style.display = "none";  // CSS
+  jsTooltip.style.display = "none";  // JavaScript
+  phpTooltip.style.display = "none";  // php
+  javaTooltip.style.display = "none";  // Java
+  mysqlTooltip.style.display = "none";  // MySQL
+  herokuTooltip.style.display = "none";  // Heroku
+  vscodeTooltip.style.display = "none";  // VSCode
+  eclipseTooltip.style.display = "none";  // Eclipse
+  androidTooltip.style.display = "none";  // Android
+  githubTooltip.style.display = "none";  // github
+  virtualboxTooltip.style.display = "none";  // virtualbox
+  vmwareTooltip.style.display = "none";  // vmware
+  slackTooltip.style.display = "none";  // Slack
+  notionTooltip.style.display = "none";  // Notion
 }
